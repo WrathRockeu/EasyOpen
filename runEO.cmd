d207937c2352e68208e71d419ab08466d3a09573
WrathRockeu <broderickciaran@gmail.com>
2016-01-02 12:59:19 +0000

    Initial commit

---
+++ runEO.cmd
@@ -0,0 +1,8 @@
+::runEO.cmd - Run EasyOpen batch script
+::To set up this file properly, copy the location of the EasyOpen.py file and replace the your_path_here with that line
+::Leave the \EasyOpen.py part of the line as is however
+SET path=your_path_here\EasyOpen.py
+::DO NOT EDIT BELOW THIS LINE
+@ECHO OFF
+call %path%
+
